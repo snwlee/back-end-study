8e7bbf2fade571046c73d7b916079177dba5a574
JunseongHeo <oksk476@naver.com>
2022-07-10 19:21:57 +0900

    :star: feat: 준성 윤년 문제

--- 스터디준비/준성/src/Test_0711/LeapYearTest.java
+++ 스터디준비/준성/src/Test_0711/LeapYearTest.java
@@ -3,33 +3,25 @@ package Test_0711;
 import java.util.Scanner;
 
 public class LeapYearTest {
-	
-	// 연도입력 메소드
-	public static int inputYear() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("윤년여부를 알고싶은 연도를 입력해주세요 >> ");
-		return scanner.nextInt();
+	public static void main(String[] args) { // 메인 메서드
+		int year = InputYear();
+		int result = resultJudge(year);
+		System.out.println(result);
 	}
-	
-	public static void main(String[] args) {
-		
-		int Year = inputYear();
-		int result;
-		
-//		조건1 : 4의 배수
-//		조건2 : 100의 배수가 아님
-//		조건3 : 400의 배수일 수 있음
-		if ( (Year%4 == 0) && (!(Year%100 == 0) || (Year%400 == 0)) ) {
-			result = 1;
-			System.out.println(result);
-		} else {
-			result = 0;
-			System.out.println(result);
+	public static int InputYear() { // 연도입력 및 입력조건 판별 메서드
+		Scanner scanner = new Scanner(System.in);
+		int inputYear = scanner.nextInt();
+		if (1 <= inputYear && inputYear <= 4000 ) {
+			return inputYear;
+		}  else {
+			System.out.println("1보다 크거나 같고 4000보다 작거나 같은 자연수를 입력해주세요.");
+			return InputYear(); 
 		}
-		
-		// 테스트코드
-//		int ayaer = year.now();
-//		int test = Year.isLeap();
-		
+	}
+	public static int resultJudge(int year) { // 윤년여부 판별 메서드
+		if (( year%4 == 0) && (!(year%100 == 0) || (year%400 == 0)) ) {
+			return 1; // 윤년
+		} 
+		return 0; // 평년
 	}
 }
