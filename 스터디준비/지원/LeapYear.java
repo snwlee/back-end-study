b10fdf169f66b8ba1094c0d28584821d5a34fea4
6time <106802716+6time@users.noreply.github.com>
2022-07-11 14:22:23 +0900

    ⭐ feat: 지원 윤년 문제

--- 스터디준비/지원/LeapYear.java
+++ 스터디준비/지원/LeapYear.java
@@ -1,1 +1,18 @@
+package studyOne;
 
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if(a%4==0 && a%100!=0 || a%400==0) {
+			System.out.println("윤년");
+		}else {
+			System.out.println("평년");
+		}
+	}
+}
