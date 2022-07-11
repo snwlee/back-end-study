ac145784624adafd8f6300c4340ae1958d3de6e0
youjin931217 <youjincjstk@naver.com>
2022-07-11 12:49:18 +0900

    ⭐ feat: 유진 윤년 문제

---
+++ 스터디준비/유진/leap year.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Leap_year {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int year = scan.nextInt();
+		
+		if(year%4 == 0 && year%100 != 0 || year%400 == 0) {
+				System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
+}
